44b91605c444d149a864f3bbbf64b0f4c28838e7
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2023-04-03 18:26:22 +0530

    Add files via upload

---
+++ WIP/ThingsToDo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorials
+{
+    internal class ThingsToDo
+    {
+        /// Idea to learn things
+        /// More than understanding what an idea is
+        /// say instead of focusing on what a class is,
+        /// try to understand Why a class concept is there in the first place
+        /// What Vs Why
+        /// What part tells us what the concept can do
+        /// Why part tells us why it was designed as a concept
+        /// this will help you grasp concepts as well help you to 
+        /// explain it to others as well as the interviewers
+
+        /// Program has more than one entry point.. what does this error mean?
+        /// Possible interview questions
+        /// Types of namespaces you know
+        /// Types of errors you know
+        /// Type-safe language ?
+        /// Difference between C# and VB.NET
+        /// Difference between C# and Python
+        /// What did you learn in clean code?
+        /// Space - Time complexity | Big O notation
+        /// Multithreading - just text description 
+        /// Stack vs Heap
+        /// Writing an easy to understand code is much more important than writing an intelligent code - Captain
+        /// Different Design patterns you know
+        /// Unit Testing
+        /// Which version of .NET framework are you using
+        /// Which version of C# are you using
+        /// Compile time vs Run time polymorphism
+        /// Value vs ref type
+        /// shallow vs deep copy
+        /// method overloading vs method overriding vs operator overloading
+        /// Different types of exceptions
+        /// Logging process
+        /// Library vs Exe
+        /// CLR
+        /// .NET framework
+        /// Using statement
+        /// Can classes be contained inside class?
+        /// Namespaces can be contained within namespaces
+
+        /// Debug vs Release mode
+        /// General project setting
+        /// string vs String
+        /// Interface (example with collections in for and foreach loop)
+        /// Unit Testing
+        /// LINQ
+
+        /// https://stackoverflow.com/questions/742341/difference-between-abstraction-and-encapsulation
+    }
+}
